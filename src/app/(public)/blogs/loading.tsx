6543e95bbcf425ac70dfcd8ebf74ba17a8d80cf1
Ayub40 <ayubk4028@gmail.com>
2025-09-24 00:42:30 +0600

    first commit

---
+++ src/app/(public)/blogs/loading.tsx
@@ -0,0 +1,13 @@
+import Loading from "@/components/ui/Loading";
+
+
+const BlogLoadingPage = () => {
+
+    return (
+        <div className="py-24">
+            <Loading />
+        </div>
+    );
+};
+
+export default BlogLoadingPage;
